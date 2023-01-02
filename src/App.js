c00fa3f521e91a9c2dd8915e55971be786969a5a
4thdraw <4thdraw2@gmail.com>
2023-01-02 21:58:30 +0900

    정렬 초기값 랜더링하기

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { useState } from 'react';
 import './App.css';
+import Quick from './component/Quick';
+import Notice from './component/Notice';
 
 function App() {
-  //useState로 선언된 변수의 값이 달라지면
-  //App컴포넌트 재랜더링한다.
-  // const open = true; 
+ 
   const [open, setToggle] = useState(true);
   return (
-    <div className={ open ? "white" : "dark" } >
-    {/* 버튼을 클릭하면 클래스명이 번갈아 가면서 들어온다.
-        */}
-        <button onClick={ () => { setToggle(!open); } }>
-           클릭하면 화면이 바뀌지게
-        </button>
-        
+    <div className={ open ? "body openpopup" : "body" }   >
+      <div id="pop">
+        <button onClick={ () => { setToggle(!open) } }>닫기</button>
+        <p>화면 정가운데</p>
+      </div> 
+      <Quick></Quick> 
+      <Notice title="공지사항"></Notice>
     </div>
   );
 }
