16c95c784acb3449e81fb3b2032f1661545f4bda
4thdraw <4thdraw2@gmail.com>
2023-01-02 19:47:59 +0900

    카운드증가해서 랜더링하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //useState로 선언된 변수의 값이 달라지면
+  //App컴포넌트 재랜더링한다.
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* 버튼을 클릭하면 count의 값을 1로 변경해라 
+        count = count + 1; count++; 에러난다.
+        */}
+        <button onClick={ () => { setCount(count + 1); } }>클릭하면 화면이 바뀌지게</button>
+        <h2 style={{fontSize : 50 }}>{count}</h2>
     </div>
   );
 }
