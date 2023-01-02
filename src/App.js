aaae6c04c1d9b4787db7e09cb624a388101e5e08
4thdraw <4thdraw2@gmail.com>
2023-01-02 20:11:10 +0900

    토글기능

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { useState } from 'react';
+import './App.css';
 
 function App() {
   //useState로 선언된 변수의 값이 달라지면
   //App컴포넌트 재랜더링한다.
-  const [count, setCount] = useState(0);
+  // const open = true; 
+  const [open, setToggle] = useState(true);
   return (
-    <div className="App">
-    {/* 버튼을 클릭하면 count의 값을 1로 변경해라 
-        count = count + 1; count++; 에러난다.
+    <div className={ open ? "white" : "dark" } >
+    {/* 버튼을 클릭하면 클래스명이 번갈아 가면서 들어온다.
         */}
-        <button onClick={ () => { setCount(count + 1); } }>클릭하면 화면이 바뀌지게</button>
-        <h2 style={{fontSize : 50 }}>{count}</h2>
+        <button onClick={ () => { setToggle(!open); } }>
+           클릭하면 화면이 바뀌지게
+        </button>
+        
     </div>
   );
 }
